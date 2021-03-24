50cfc262e0d7a9113582a48c8f614178c060f8a5
inamumu <qty7.xbcrk@gmail.com>
2021-03-24 23:27:07 +0900

    共通ヘッダー作成

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,24 @@
+@section('header')
+<div class="header_inner">
+  <div class="head01">
+    <h1>Logo</h1>
+  </div>
+  <nav class="nav">
+    <div class="toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+    <div class="global_nav">
+      <ul>
+        <li><a href="">Register</a></li>
+        <li><a href="">Login</a></li>
+        <li><a href="">MyPage</a></li>
+        <li><a href="">Post</a></li>
+        <li><a href="">Logout</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
+@endsection
